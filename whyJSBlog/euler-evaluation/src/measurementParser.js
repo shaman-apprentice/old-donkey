4d6e81d81a5be7bbb6028fef70d398e8ad72f128
shaman-apprentice <Torsten.Knauf@gmx.de>
2021-01-09 20:59:48 +0100

    Start customization

--- whyJSBlog/euler-evaluation/src/measurementParser.js
+++ whyJSBlog/euler-evaluation/src/measurementParser.js
@@ -19,7 +19,7 @@ const parseProblem = (headingLineRegExpForProblem) => {
 /** @param headingRegExpBuilder - function taken number as parameter and returning a RegExp matching problem's heading
  *  @return [] - average taken time per problem, mapped to x and y axes
  */
-export const parseProblemsForLanguage = (headingRegExpBuilder, start, end) => {
+const parseProblemsForLanguage = (headingRegExpBuilder, start, end) => {
   const data = []
   for (let i = start; i <= end; i++) {
     // if (i === 5 || i === 10)
@@ -31,3 +31,54 @@ export const parseProblemsForLanguage = (headingRegExpBuilder, start, end) => {
 }
 
 console.log(parseProblem((number) => new RegExp(`${number}/main.js`)))
+
+export const createChartObjectForProb1To10 = () => ({
+  type: 'line',
+  data: {
+    labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+    datasets: [
+      {
+        label: 'C',
+        data: parseProblemsForLanguage((number) => new RegExp(`${number}/main\\.exe`), 1, 10),
+        borderColor: "rgb(205,133,63)",
+      },
+      {
+        label: 'Java',
+        data: parseProblemsForLanguage((number) => new RegExp(`${number}/Main\\.java`), 1, 10),
+        borderColor: "rgb(255,0,0)",
+      },
+      {
+        label: 'JavaScript',
+        data: parseProblemsForLanguage((number) => new RegExp(`${number}/main\\.js`), 1, 10),
+        borderColor: "rgb(255,215,0)",
+      },
+      {
+        label: 'Python',
+        data: parseProblemsForLanguage((number) => new RegExp(`python.*${number}\\.main`), 1, 10),
+        borderColor: "rgb(75, 75, 192)",
+      },
+    ]
+  },
+  options: {
+    title: { display: true, text: "1. All" },
+    responsive: false,
+    animation: false,
+    scales: {
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: "problem number"
+          },
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: "t/s in average with 100 runs"
+          }
+        }],
+    },
+    elements: {
+      line: { fill: false }
+    }
+  }
+})
