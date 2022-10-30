e71a06062b6c0bbfbf0eb90b5ec77b8995035ac9
shaman-apprentice <Torsten.Knauf@gmx.de>
2022-10-30 18:30:22 +0100

    add dotted lines for unknown other team

--- Seriuos Bad source code Again Blog/thoughtExperimentCharts/src/thrdThoughtExperiment.js
+++ Seriuos Bad source code Again Blog/thoughtExperimentCharts/src/thrdThoughtExperiment.js
@@ -8,6 +8,20 @@ export const thrdThoughtExperiment = {
       {
         label: 'Team ?',
         borderColor: "rgb(0,0,255)",
+        data: releaseValue2ChartData([0, 1, 1, 1, 1]),
+        borderDash: [5, 7],
+        pointStyle: "line"
+      },
+      {
+        borderColor: "rgb(0,0,255)",
+        data: releaseValue2ChartData([2, 3, 2, 2, 2]),
+        borderDash: [5, 7],
+        pointStyle: "line"
+      },{
+        borderColor: "rgb(0,0,255)",
+        data: releaseValue2ChartData([0, -0.5, -0.4, -0.3, -0.2]),
+        borderDash: [5, 7],
+        pointStyle: "line"
       },
       {
         label: 'Team clean source code',
@@ -20,6 +34,11 @@ export const thrdThoughtExperiment = {
     title: { display: true, text: "Product development with competition taking market share" },
     responsive: false,
     animation: false,
+    legend: {
+      labels: {
+        filter: (legendItem) => legendItem.text !== undefined 
+      }
+    }, 
     scales: {
         xAxes: [{
           scaleLabel: {
@@ -32,7 +51,7 @@ export const thrdThoughtExperiment = {
             display: true,
             labelString: "Total earned money"
           },
-          ticks: { min: 0, max: 16  }
+          ticks: { min: -2, max: 16  }
         }],
     },
     elements: {
